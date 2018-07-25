18a9ce524c3908c3810384ce5dcc7a4871bd3ddd
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-07-25 22:25:19 +0200

    dodanie application

---
+++ application.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+class Application:
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://forum.attnauka.webd.pro/")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+
+        # wpisywanie użytkownika
+        wd.find_element_by_name("username").click()
+        wd.find_element_by_name("username").send_keys(username)
+
+        # wpisywanie hasła
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").send_keys(password)
+
+        # potwierdzanie logowania
+        wd.find_element_by_name("login").click()
+
+    def destroy(self):
+        self.wd.quit()
