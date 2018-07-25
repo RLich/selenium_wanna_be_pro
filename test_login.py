9c0bede28ffdc73cbfe588b284347855bc8308a2
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-07-25 22:19:55 +0200

    test commita

--- test_login.py
+++ test_login.py
@@ -1,1 +1,15 @@
-asdasdasdasd
+import pytest
+from selenium.webdriver.chrome.webdriver import WebDriver
+from application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_login(app):
+    app.login(username="rafal", password="rafaltestowy")
+
+
