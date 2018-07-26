f8fa76ace5b0b45b587e366895db174fb9388bd3
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-07-26 22:40:31 +0200

    assert 3

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ class Application:
 
     def get_username_from_nav_bar(self):
         wd = self.wd
-        wd.find_element_by_id("username_logged_in").text
+        wd.find_element_by_id("username_logged_in")
         return self
 
     def logout(self):
