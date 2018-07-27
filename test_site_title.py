5f649968a338d6f8874bcef9a486163ffb959237
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-07-28 01:50:47 +0200

    test_site_title proba unittest trzeci fail

--- test_site_title.py
+++ test_site_title.py
@@ -10,4 +10,4 @@ def app(request):
 
 def test_site_title(app):
     app.open_home_page()
-    assert app.get_site_title == "ATT Nauka - Index page"
+    app.get_site_title.assertIn("ATT Nauka - Index page", app.get_site_title)
