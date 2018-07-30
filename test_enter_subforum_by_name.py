6fbfb18651df1d5ffe85dd4ad2962d1ca58e59ea
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-07-31 00:25:44 +0200

    enter_subforum działające, nieuporządkowane

--- test_enter_subforum_by_name.py
+++ test_enter_subforum_by_name.py
@@ -11,11 +11,14 @@ driver.find_element_by_name("password").click()
 driver.find_element_by_name("password").send_keys(Config.password)
 driver.find_element_by_name("login").click()
 
-element = driver.find_elements_by_class_name('forumtitle')
-subforum = driver.find_element_by_class_name('forumtitle')
-for subforum in element:
-    subforum.text == 'Konrad'
-    subforum.click()
+elements = driver.find_elements_by_class_name('forumtitle')
+for elements[0] in elements:
+    if elements[0].text == 'Konrad':
+        elements[0].click()
+        break
+assert driver.find_element_by_class_name('forum-title').text == 'Konrad'
+
+
 time.sleep(3)
 
 driver.quit()
