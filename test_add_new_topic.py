2782661ffd15702f10f3321b7fd982c8a887c5e8
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-07-31 01:22:09 +0200

    test_add_new_topic 1

---
+++ test_add_new_topic.py
@@ -0,0 +1,21 @@
+import pytest
+from application import Application
+from Config.cfg_att import Config
+import time
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_topic(app):
+    app.login(Config.username, Config.password)
+    app.enter_subforum_by_name(name="Marcin")
+    assert app.wd.find_element_by_class_name('forum-title').text == 'Marcin'
+    app.wd.find_element_by_xpath("//*[@title='Post a new topic']").click()
+    app.wd.fid_element_by_name("subject").click()
+    app.wd.fid_element_by_name("subject").send_keys("test1")
+    time.sleep(2)
+
