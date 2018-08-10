7418da648c165b0c4199b6df96aa4bdeb5013bd4
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-08-10 17:24:59 +0200

    Pogrom kodu - in progress

--- brudnopis.py
+++ brudnopis.py
@@ -12,5 +12,5 @@ def app(request):
 def test_brudnopis(app):
     app.login(Config.username2, Config.password2)
     app.enter_private_messages()
-    list = app.wd.find_elements_by_class_name("topictitle")
-    print(list.count("topictitle"))
+    list_of_topics = app.wd.find_elements_by_class_name("topictitle")
+    print(len(list_of_topics.count("topictitle")))
