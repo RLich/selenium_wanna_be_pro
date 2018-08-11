8c71efbc79829b8bd64707a9c1be6e3e901d09ea
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-08-11 12:45:20 +0200

    adding test description - in progress

--- test_search_by_title.py
+++ test_search_by_title.py
@@ -14,6 +14,10 @@ def test_earch_by_title(app):
     topic_title = "Unikalny temat dla testu wyszukiwarki"
     topic_text = app.random_string(10, 30)
     subforum_name = "Rafał"
+
+    # logowanie użytkownika, przejście do wybranego subforum
+    # stworzenie nowego tematu i wyszukanie go po tytule w wyszukiwarce headbaru
+    # potwierdzenie znalezienia zdefiniowanego tematu po tytule
     app.login(Config.username1, Config.password1)
     app.enter_subforum_by_name(subforum_name)
     app.create_new_topic(topic_title, topic_text)
