0862968db09a19524e80dbcd54b0606530095a67
Mortis7 <41342211+Mortis7@users.noreply.github.com>
2018-08-11 12:27:43 +0200

    adding test description - in progress

--- test_add_new_post.py
+++ test_add_new_post.py
@@ -15,6 +15,10 @@ def test_add_new_post(app):
     subforum_name = "Marcin"
     topic_text = app.random_string(10, 30)
     reply = app.random_string(10, 20)
+
+    # logowanie użytkownika, przejście do wybranego subforum
+    # stworzenie tematu oraz wysłanie odpowiedzi w założonym wątku
+    # potwierdzenie zgodności przesłanej odpowiedzi z zakładaną treścią
     app.login(Config.username1, Config.password1)
     app.enter_subforum_by_name(subforum_name)
     app.create_new_topic(topic_title, topic_text)
